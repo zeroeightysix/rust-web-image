dc4161003166dcfc5f6caa765fcaeac524171bd2
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-10 01:58:06 +0100

    deny_out_of_range!

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,27 @@ use rocket::response::Content;
 use image::RgbaImage;
 use crate::payload::IntoVec;
 
-#[post("/blur/<sigma>", data = "<image>")]
+macro_rules! deny_out_of_range {
+    ($expression:ident, $range:expr) => {
+        if !$range.contains(&$expression) {
+            return Err(Status::Forbidden)
+        }
+    };
+}
+
+#[post("/blur?<sigma>", data = "<image>")]
 fn blur(image: Data, sigma: f32) -> Result<Content<Vec<u8>>, Status> {
     let (image, _) = payload::image_from_data(image)?;
-    if sigma <= 0.0 || sigma > 10.0 {
-        return Err(Status::Forbidden)
-    }
+    deny_out_of_range!(sigma, 0.0 .. 10.0);
 
     let image: RgbaImage = imageproc::filter::gaussian_blur_f32(&image.into_rgba8(), sigma);
     Ok(Content(ContentType::PNG, image.as_vec()?))
 }
 
-#[post("/unsharpen?<sigma>&<threshold>", data="<image>")]
+#[post("/unsharpen?<sigma>&<threshold>", data = "<image>")]
 fn unsharpen(image: Data, sigma: f32, threshold: i32) -> Result<Content<Vec<u8>>, Status> {
     let (image, _) = payload::image_from_data(image)?;
-    if sigma <= 0.0 || sigma > 10.0 {
-        return Err(Status::Forbidden)
-    }
+    deny_out_of_range!(sigma, 0.0 .. 10.0);
 
     let image = image.unsharpen(sigma, threshold);
     Ok(Content(ContentType::PNG, image.as_vec()?))
